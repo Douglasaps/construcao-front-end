a77c097e981b4c47c8fdaeb22458682fc25c7dc8
unknown <2324290114@ACADEMICO.IESB.BR>
2024-08-29 21:41:13 -0300

    adicionando aula 03

---
+++ aulas/aula03/src/App.jsx
@@ -0,0 +1,17 @@
+function Cabecalho() {
+  return (
+    <header>
+      <h1>Minha Aplicação</h1>
+    </header>
+  )
+}
+
+function App() {
+ return (
+  <div>
+  <Cabecalho/>
+  <p>Bem Vindo!!</p>
+  </div>
+ );
+}
+export default App;
